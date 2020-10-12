d0189bf4d3f5ab5eb8dc3e01350acfd1e4b4fd17
Mark Erickson <mje314@Marks-MacBook-Air.local>
2020-10-12 12:09:36 -0500

    added circles to map

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,14 +15,49 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(myMap);
 
 // api call to get geo json data on earthquakes
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(createMarkers);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson").then(createMarkers);
 
 // function to create markers
 function createMarkers(response) {
     
     // console the response 
-    console.log(response);
-    //
+    console.log(response.features);
+    
+    // loop through earthquakes and create markers
+    for (var i = 0; i < response.features.length; i++) {
+
+        // conditionals for color of circle based on depth
+        var color = "";
+        var depth = response.features[i].geometry.coordinates[2];
+        if (depth < 5) {
+            color = "green";
+        }
+
+        else if (depth < 10) {
+            color = "yellow";
+        }
+
+        else if (depth < 20) {
+            color = "orange";
+        }
+
+        else {
+            color = "red";
+        }
+
+        // add circles to the map
+        var mag = response.features[i].properties.mag;
+        var lat = response.features[i].geometry.coordinates[1];
+        var lon = response.features[i].geometry.coordinates[0];
+        
+        L.circle([lat, lon], {
+            fillOpacity: 0.75,
+            color: "black",
+            fillColor: color,
+            radius: mag * 25000
+        }).addTo(myMap);
+
+    }
 
 
 
